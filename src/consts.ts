09e4adfef136d37c9a3b78816f2d6cecd619c421
JJVNS <joshuajesivenus@gmail.com>
2026-06-14 22:56:48 +0800

    Update: Apply Automotive-Authentic palette and responsive polish

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SouthShift';
-export const SITE_DESCRIPTION = 'The Ultimate Automotive Guide and Marketplace for South Luzon.';
+export const SITE_TITLE = 'Apex Engine';
+export const SITE_DESCRIPTION = 'The Ultimate Automotive Engineering Guide and Marketplace.';
